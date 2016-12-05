ea2fe3bbd3bd65d9da974ba329fb93e776805007
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-12-05 14:52:55 +0100

    minor update

--- libs/build.sh
+++ libs/build.sh
@@ -5,14 +5,16 @@ git clone --recursive -j4 https://github.com/OSSIA/API
 mkdir build
 (
 cd build
-cmake ../API -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=ossia -DOSSIA_TESTING=0 -DOSSIA_STATIC=0 -DOSSIA_NO_SONAME=1
-make -j8
+cmake ../API -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=ossia -DOSSIA_TESTING=0 -DOSSIA_STATIC=0 -DOSSIA_NO_SONAME=1 -DOSSIA_PD=0
+make -j$(nproc)
 make install
 rm -rf ../ossia/include
 mv ossia/include ../ossia/include
 if [[ -d "/proc" ]] ; then
+  mkdir -p ../ossia/lib/linux64
   mv ossia/lib/libossia.so* ../ossia/lib/linux64/
 else
+  mkdir -p ../ossia/lib/osx
   mv ossia/lib/libossia.*dylib* ../ossia/lib/osx/
 fi
 )
--- libs/ossia/include/ossia/prefix.hpp
+++ libs/ossia/include/ossia/prefix.hpp
@@ -8,8 +8,8 @@
 #include <vector>
 #include <list>
 #include <memory>
-#include <unordered_map>
 #include <functional>
+#include <hopscotch_map.h>
 #include <string>
 #include <ossia/detail/config.hpp>
 #include <ossia/detail/algorithms.hpp>
