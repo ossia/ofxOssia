137e81876799ca5c6650e024af35d5fad9aa4cab
Antoine Villeret <antoine.villeret@gmail.com>
2017-05-30 12:21:58 +0200

    remove unused functions randomize initial values

--- example-simple/src/InteractiveCircle.cpp
+++ example-simple/src/InteractiveCircle.cpp
@@ -13,17 +13,17 @@ void InteractiveCircle::setup(ossia::ParameterGroup _parent_node){
     _circleParams.setup(_parent_node, "circle");
     
     _sizeParams.setup(_circleParams, "sizeParams");
-    _radius.setup(_sizeParams,"radius",10.,1.,100.);
+    _radius.setup(_sizeParams,"radius",ofRandomf()*99+1,1.,100.);
     _position.setup(_sizeParams,
                     "position",
-                    ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2),
+                    ofVec2f(ofRandomWidth(), ofRandomHeight()),
                     ofVec2f(0., 0.), // Min
                     ofVec2f(ofGetWidth(), ofGetHeight())); // Max
     
     _colorParams.setup(_circleParams, "colorParams");
     _color.setup(_colorParams,
                  "color",
-                 ofColor(123., 255., 17., 255.),
+                 ofColor(ofRandom(255), ofRandom(255), ofRandom(255), 255.),
                  ofColor(0., 0., 0., 0.),
                  ofColor(255., 255., 255., 255.));
     _fill.setup(_colorParams,"fill",false);
