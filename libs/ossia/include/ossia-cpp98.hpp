25305911d232e371201dc18af3323def2d25d99e
Pascal Baltazar <pascal@baltazars.org>
2018-03-19 15:21:56 +0100

    updating include and git-ignoring lib

--- libs/ossia/include/ossia-cpp98.hpp
+++ libs/ossia/include/ossia-cpp98.hpp
@@ -41,9 +41,9 @@ class oscquery_mirror;
 class OSSIA_EXPORT value
 {
 public:
-  using vec2f = std::array<float, 2ul>;
-  using vec3f = std::array<float, 3ul>;
-  using vec4f = std::array<float, 4ul>;
+  typedef std::array<float, 2ul> vec2f ;
+  typedef std::array<float, 3ul> vec3f ;
+  typedef std::array<float, 4ul> vec4f ;
 
   value();
   ~value();
@@ -114,6 +114,7 @@ struct callback_index {
     ~callback_index();
     callback_index(const callback_index&);
     callback_index& operator=(const callback_index&);
+ //   explicit operator bool() const;
 
   private:
     friend class node;
@@ -141,6 +142,7 @@ public:
 
   node find_child(std::string addr);
   void remove_child(std::string addr);
+  void remove_children();
 
   // Create a node
   node create_child(std::string addr);
@@ -301,7 +303,7 @@ private:
   void cleanup_parameter(const ossia::net::parameter_base&);
 
   ossia::net::node_base* m_node;
-  ossia::net::parameter_base* m_addr;
+  ossia::net::parameter_base* m_param;
 };
 
 class OSSIA_EXPORT oscquery_server
